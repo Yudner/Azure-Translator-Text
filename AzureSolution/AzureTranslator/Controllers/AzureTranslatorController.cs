008e2fbe64a9a92b370ec74f9a456806eaff1224
Yudner Paredes <yudner.paredes@hotmail.com>
2021-12-02 12:38:51 -0500

    add solution net core 3

---
+++ AzureSolution/AzureTranslator/Controllers/AzureTranslatorController.cs
@@ -0,0 +1,27 @@
+﻿using Infrastructure.AzureTranslator;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AzureTranslator.Controllers
+{
+    [ApiController]
+    [Route("api/translator")]
+    public class AzureTranslatorController: ControllerBase
+    {
+        private IAzureTranslatorService _azureTranslatorService;
+        public AzureTranslatorController(IAzureTranslatorService azureTranslatorService)
+        {
+            _azureTranslatorService = azureTranslatorService;
+        }
+
+        [HttpPost("text")]
+        public async Task<IActionResult> TranslatorText([FromBody] List<AzureTranslatorRequestBody> body)
+        {
+            var result = await _azureTranslatorService.Execute(body);
+            return Ok(result);
+        }
+    }
+}
